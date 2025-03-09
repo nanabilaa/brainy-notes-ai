dfe182d6ec1216171953e0457bb49811da4778bd
Nabila <putriiynabila@gmail.com>
2025-03-09 11:44:53 +0700

    90325

---
+++ script.js
@@ -0,0 +1,15 @@
+// Inisialisasi AOS
+AOS.init({
+    duration: 600, 
+    easing: 'ease-out', 
+    once: false, 
+    mirror: true, });
+
+// Event
+document.getElementById("tryFreeBtn").addEventListener("click", function () {
+    window.location.href = "trial.html";
+});
+
+document.getElementById("tryNowBtn").addEventListener("click", function () {
+    window.location.href = "main.html";
+});
